d7db78a86c01babf36a304632574a007e0c78ec9
wonyoon <62367065+wonyoon@users.noreply.github.com>
2020-03-27 21:46:16 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!, Jenkins + Docker test");
 });
-
 const port = process.env.PORT || 1337;
 server.listen(port);
 
